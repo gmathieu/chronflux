0f9910683fe387661140057149f610fa3505a6fe
Guillaume Mathieu <contact@guillaumemathieu.com>
2011-08-10 19:01:07 +0000

    added ability to create a project

--- application/models/Project.php
+++ application/models/Project.php
@@ -1,4 +1,12 @@
 <?php
 class App_Model_Project extends Mg_Data_Object
 {
+    public static function create($data)
+    {
+        $project = new self($data);
+
+        App_Model_Projects::getInstance()->insert($project);
+
+        return $project;
+    }
 }
---
+++ application/models/Projects.php
@@ -0,0 +1,4 @@
+<?php
+class App_Model_Projects extends Mg_Data_Service
+{
+}
