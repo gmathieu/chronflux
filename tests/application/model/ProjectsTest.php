3b354e42eb73b4a291b4ffaf1ad651f1e2e5d371
Guillaume Mathieu <contact@guillaumemathieu.com>
2011-08-10 22:04:38 +0000

    changed project's canDelete logic to check number of associated users

--- tests/application/model/ProjectsTest.php
+++ tests/application/model/ProjectsTest.php
@@ -5,10 +5,12 @@ class ProjectsTest extends AbstractDatabaseTestCase
     const PROJECT_1_ID          = 1;
     const PROJECT_2_ID          = 2;
     const PROJECT_3_ID          = 3;
+    const PROJECT_4_ID          = 4;
 
-    const PROJECT_1_TOTAL_HOURS = 16;
-    const PROJECT_2_TOTAL_HOURS = 8;
-    const PROJECT_3_TOTAL_HOURS = 0;
+    const PROJECT_1_TOTAL_USERS = 1;
+    const PROJECT_2_TOTAL_USERS = 1;
+    const PROJECT_3_TOTAL_USERS = 2;
+    const PROJECT_4_TOTAL_USERS = 0;
 
     public $projects;
 
@@ -21,16 +23,19 @@ class ProjectsTest extends AbstractDatabaseTestCase
         $this->projects = App_Model_Projects::getInstance();
     }
 
-    public function testTotalHours()
+    public function testTotalUsers()
     {
         $project = $this->projects->find(self::PROJECT_1_ID);
-        $this->assertEquals(self::PROJECT_1_TOTAL_HOURS, $project->project_total_hours);
+        $this->assertEquals(self::PROJECT_1_TOTAL_USERS, $project->total_users);
 
         $project = $this->projects->find(self::PROJECT_2_ID);
-        $this->assertEquals(self::PROJECT_2_TOTAL_HOURS, $project->project_total_hours);
+        $this->assertEquals(self::PROJECT_2_TOTAL_USERS, $project->total_users);
 
         $project = $this->projects->find(self::PROJECT_3_ID);
-        $this->assertEquals(self::PROJECT_3_TOTAL_HOURS, $project->project_total_hours);
+        $this->assertEquals(self::PROJECT_3_TOTAL_USERS, $project->total_users);
+
+        $project = $this->projects->find(self::PROJECT_4_ID);
+        $this->assertEquals(self::PROJECT_4_TOTAL_USERS, $project->total_users);
     }
 
     public function testCanDelete()
@@ -42,6 +47,9 @@ class ProjectsTest extends AbstractDatabaseTestCase
         $this->assertFalse($project->canDelete());
 
         $project = $this->projects->find(self::PROJECT_3_ID);
+        $this->assertFalse($project->canDelete());
+
+        $project = $this->projects->find(self::PROJECT_4_ID);
         $this->assertTrue($project->canDelete());
     }
 }
