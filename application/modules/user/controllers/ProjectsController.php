23e47b17fef27bc0d6ba5e06efc206da93f41a94
Guillaume Mathieu <contact@guillaumemathieu.com>
2011-09-04 11:52:54 -0400

    added context switching to user project list

--- application/modules/user/controllers/ProjectsController.php
+++ application/modules/user/controllers/ProjectsController.php
@@ -6,6 +6,11 @@ class User_ProjectsController extends App_User_Controller_Action
     {
         parent::init();
 
+        // context switch
+        $this->_helper->getHelper('AjaxContext')
+                      ->addActionContext('list', 'html')
+                      ->initContext();
+
         // init projects service
         $this->projects = User_Model_Projects::getInstance();
         $this->projects->setUserId($this->user->id);
