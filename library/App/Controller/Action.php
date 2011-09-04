6e570791f7f322c4f9166d6d486ecba69c72a2f9
Guillaume Mathieu <contact@guillaumemathieu.com>
2011-09-04 11:51:42 -0400

    added grid toggled by GET param

--- library/App/Controller/Action.php
+++ library/App/Controller/Action.php
@@ -2,4 +2,11 @@
 
 class App_Controller_Action extends Zend_Controller_Action
 {
+    public function init()
+    {
+        if ($this->_getParam('show-grid')) {
+            $url = $this->view->baseUrl('images/24_col.gif');
+            $this->view->headStyle()->appendStyle("html { background: #ffffff url({$url}) -5px 0 repeat; }");
+        }
+    }
 }
