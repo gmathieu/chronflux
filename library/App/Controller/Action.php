a61f11bbe324dd493daf667ac42bb6807e60d24a
Guillaume Mathieu <contact@gmathieu.com>
2011-09-07 14:03:03 -0400

    added controller and action names to view added isAjax function helper

--- library/App/Controller/Action.php
+++ library/App/Controller/Action.php
@@ -2,11 +2,28 @@
 
 class App_Controller_Action extends Zend_Controller_Action
 {
+    public $controllerName;
+    public $actionName;
+
     public function init()
     {
+        $this->controllerName = $this->getRequest()->getControllerName();
+        $this->actionName     = $this->getRequest()->getActionName();
+
         if ($this->_getParam('show-grid')) {
             $url = $this->view->baseUrl('images/24_col.gif');
             $this->view->headStyle()->appendStyle("html { background: #ffffff url({$url}) -5px 0 repeat; }");
         }
+    }
+
+    public function preDispatch()
+    {
+        $this->view->controllerName = $this->controllerName;
+        $this->view->actionName     = $this->actionName;
+    }
+
+    public function isAjax()
+    {
+        return $this->getRequest()->isXmlHttpRequest();
     }
 }
