8a1d7bfe6dc4384f5504d29971add76369834d9e
Guillaume Mathieu <contact@gmathieu.com>
2011-09-17 12:34:51 +0200

    now using a span instead of a div for the clear helper

--- application/views/helpers/Clear.php
+++ application/views/helpers/Clear.php
@@ -5,7 +5,7 @@ class View_Helper_Clear extends Mg_View_Helper_AbstractHelper
     public function clear()
     {
         return <<<HTML
-        <div class="clear">&nbsp;</div>
+        <span class="clear">&nbsp;</span>
 HTML;
     }
 }
