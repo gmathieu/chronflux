5b185b8ca386da7de54e025ea4c5835860321c82
Guillaume Mathieu <contact@gmathieu.com>
2011-09-19 14:49:25 +0200

    abstracted out common functionality into settings controller, moved user data set list to setting's layout

--- application/modules/user/controllers/ProjectsController.php
+++ application/modules/user/controllers/ProjectsController.php
@@ -2,76 +2,9 @@
 
 class User_ProjectsController extends App_User_Controller_Settings
 {
-    public $userProjects;
-    public $userProjectSet;
-
-    public function init()
-    {
-        parent::init();
-
-        // context switch
-        $this->_helper->getHelper('AjaxContext')
-                      ->addActionContext('list', 'html')
-                      ->initContext();
-
-        // init projects service
-        $this->userProjects = User_Model_Projects::getInstance();
-        $this->userProjects->setUserId($this->user->id);
-
-        // get a list of projects
-        $this->userProjectSet = $this->userProjects->fetchAll();
-    }
-
-    public function preDispatch()
-    {
-        parent::preDispatch();
-
-        // always show list of user projects
-        $this->view->userProjectSet = $this->userProjectSet;
-    }
-
-    public function listAction()
-    {
-    }
-
-    public function newAction()
-    {
-        $form = $this->_initForm();
-
-        if ($this->_request->isPost() && $form->isValid($this->_request->getPost())) {
-            $userProject = User_Model_Project::create($this->_request->getPost());
-            return $this->_redirectToEditAction($userProject);
-        }
-    }
-
-    public function editAction()
-    {
-        $userProject = $this->_requireProject();
-        $form        = $this->_initForm();
-
-        // pre-populate form with user project's data
-        $form->populate($userProject->getRawData());
-
-        if ($this->_request->isPost() && $form->isValid($this->_request->getPost())) {
-            // update user project with POST data
-            $userProject->setFromArray($this->_request->getPost());
-            $this->userProjects->update($userProject);
-        }
-
-        $this->view->userProject = $userProject;
-    }
-
-    public function deleteAction()
-    {
-        $userProject = $this->_requireProject();
-        $userProject->delete();
-
-        return $this->_redirect("user/{$this->user->username}/projects");
-    }
-
     public function activateAction()
     {
-        $userProject = $this->_requireProject();
+        $userProject = $this->_requireDataObj();
         $userProject->activate();
 
         return $this->_redirectToEditAction($userProject);
@@ -79,38 +12,9 @@ class User_ProjectsController extends App_User_Controller_Settings
 
     public function deactivateAction()
     {
-        $userProject = $this->_requireProject();
+        $userProject = $this->_requireDataObj();
         $userProject->deactivate();
 
         return $this->_redirectToEditAction($userProject);
-    }
-
-    private function _requireProject()
-    {
-        $projectId   = $this->_getParam('project_id');
-        $userProject = $this->userProjects->findByProjectId($projectId);
-
-        if ($userProject) {
-            return $userProject;
-        } else {
-            throw new Exception("Project {$projectId} not found.");
-        }
-    }
-
-    private function _initForm()
-    {
-        $form = new User_Form_Project();
-
-        // force user_id
-        $form->user_id->setValue($this->user->id);
-
-        $this->view->form = $form;
-
-        return $form;
-    }
-
-    private function _redirectToEditAction($userProject)
-    {
-        return $this->_redirect("user/{$this->user->username}/projects/edit/project_id/{$userProject->project_id}");
     }
 }
--- library/App/User/Controller/Settings.php
+++ library/App/User/Controller/Settings.php
@@ -2,11 +2,28 @@
 
 class App_User_Controller_Settings extends App_User_Controller_Action
 {
+    public $dataObjName;
+    public $dataService;
+    public $userDataSet;
+
     public function init()
     {
         parent::init();
 
         $this->view->headLink()->appendStylesheet($this->view->baseUrl('css/settings.css'));
+
+        if ('projects' === $this->controllerName || 'tasks' === $this->controllerName) {
+            // init data object info
+            $this->dataObjName = ucfirst(trim($this->controllerName, 's'));
+    
+            // init data service based on controller name
+            $dataServiceObj    = "User_Model_{$this->dataObjName}s";
+            $this->dataService = $dataServiceObj::getInstance();
+            $this->dataService->setUserId($this->user->id);
+    
+            // get data set
+            $this->userDataSet = $this->dataService->fetchAll();
+        }
     }
 
     public function preDispatch()
@@ -18,6 +35,9 @@ class App_User_Controller_Settings extends App_User_Controller_Action
             $this->getHelper('viewRenderer')->setNoRender();
         }
 
+        // setup view variables
+        $this->view->dataObjName      = $this->dataObjName;
+        $this->view->userDataSet      = $this->userDataSet;
         $this->view->selectedMenuItem = 'settings';
     }
 
@@ -25,8 +45,98 @@ class App_User_Controller_Settings extends App_User_Controller_Action
     {
         parent::postDispatch();
 
+        // capture current content
         $this->view->content = $this->view->render("{$this->controllerName}/{$this->actionName}.phtml");
 
+        // render settings menu
         $this->getHelper('viewRenderer')->renderScript('settings/_layout.phtml');
+    }
+
+    public function listAction()
+    {
+    }
+
+    public function newAction()
+    {
+        $form = $this->_initForm();
+
+        if ($this->_request->isPost() && $form->isValid($this->_request->getPost())) {
+            $userDataObjName = "User_Model_{$this->dataObjName}";
+            $userDataObj     = $userDataObjName::create($this->_request->getPost());
+
+            return $this->_redirectToEditAction($userDataObj);
+        }
+    }
+
+    public function editAction()
+    {
+        $userDataObj = $this->_requireDataObj();
+        $form        = $this->_initForm();
+
+        // pre-populate form
+        $form->populate($userDataObj->getRawData());
+
+        if ($this->_request->isPost() && $form->isValid($this->_request->getPost())) {
+            // update user project with POST data
+            $userDataObj->setFromArray($this->_request->getPost());
+            $this->userProjects->update($userDataObj);
+        }
+
+        $this->view->deleteUrl   = $this->_getDeleteUrl($userDataObj);
+        $this->view->userDataObj = $userDataObj;
+        $this->view->inlineEditing()->setForm($form)->setDataObject($userDataObj);
+    }
+
+    public function deleteAction()
+    {
+        $userDataObj = $this->_requireDataObj();
+        $userDataObj->delete();
+
+        return $this->_redirect("user/{$this->user->username}/{$this->controllerName}");
+    }
+
+    private function _requireDataObj()
+    {
+        $id          = $this->_getParam('id');
+        $findMethod  = "findBy{$this->dataObjName}Id";
+        $userDataObj = $this->dataService->$findMethod($id);
+
+        if ($userDataObj) {
+            return $userDataObj;
+        } else {
+            throw new Exception("{$this->dataObjName} {$id} not found.");
+        }
+    }
+
+    private function _initForm()
+    {
+        $formName = "User_Form_{$this->dataObjName}";
+        $form = new $formName;
+
+        // force user_id
+        $form->user_id->setValue($this->user->id);
+
+        $this->view->form = $form;
+
+        return $form;
+    }
+
+    private function _getRedirectUrl()
+    {
+        return "user/{$this->user->username}"
+               . "/{$this->controllerName}/edit"
+               . "/id/{$dataObj->getId()}";
+    }
+
+    private function _getDeleteUrl($userDataObj)
+    {
+        return "user/{$this->user->username}"
+               . "{$this->controllerName}/delete"
+               . "/id/{$userDataObj->getId()}";
+    }
+
+    private function _redirectToEditAction($dataObj)
+    {
+        return $this->_redirect($this->_getRedirectUrl());
     }
 }
