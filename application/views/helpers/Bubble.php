4aeda0f7aee7b324ffcf6893e10b6b13d5e2a5fb
Guillaume Mathieu <contact@gmathieu.com>
2011-09-19 17:47:10 +0200

    added bubble view helper

---
+++ application/views/helpers/Bubble.php
@@ -0,0 +1,20 @@
+<?php
+
+class View_Helper_Bubble extends Mg_View_Helper_AbstractHelper
+{
+    public function bubble($color, $selected = false)
+    {
+        $this->setAttributes(array(
+            'class'      => 'bubble',
+            'data-color' => $color,
+        ));
+
+        if ($selected) {
+            $this->addAttribute('style', "background-color: #{$color}; color: #{$color}");
+        }
+
+        return <<<HTML
+        <span {$this->renderAttributes()}>&nbsp;</span>
+HTML;
+    }
+}
