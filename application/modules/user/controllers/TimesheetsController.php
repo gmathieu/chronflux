4d641b5b5e14b7e731defb352468660a52b4a728
Guillaume Mathieu <contact@gmathieu.com>
2011-10-24 21:16:02 -0700

    pulling saved jobs up front

--- application/modules/user/controllers/TimesheetsController.php
+++ application/modules/user/controllers/TimesheetsController.php
@@ -13,9 +13,11 @@ class User_TimesheetsController extends App_User_Controller_Action
 
     public function manageAction()
     {
-        $date         = $this->_getParam('date');
-        $userProjects = User_Model_Projects::getInstance();
-        $userTasks    = User_Model_Tasks::getInstance();
+        $date               = $this->_getParam('date');
+        $userProjects       = User_Model_Projects::getInstance();
+        $userTasks          = User_Model_Tasks::getInstance();
+        $jobs               = App_Model_Jobs::getInstance();
+        $userProjectsLookup = array();
 
         // make sure date is valid
         if (!$date) {
@@ -31,12 +33,31 @@ class User_TimesheetsController extends App_User_Controller_Action
         // set proper user
         $userTasks->setUserId($this->user->id);
         $userProjects->setUserId($this->user->id);
+        $jobs->setUserId($this->user->id);
+
+        // get all user tasks
+        $userTaskSet = $userTasks->fetchAll();
 
         // get all user projects
         $userProjectSet = $userProjects->fetchByDateOrActive($date);
 
-        // get all user tasks
-        $userTaskSet = $userTasks->fetchAll();
+        // get all user jobs for this date
+        $jobs->setDate($date);
+        $userJobs = $jobs->fetchAll();
+
+        // generate user projects lookup
+        foreach ($userProjectSet as $userProject) {
+            $userProjectsLookup[$userProject->id] = $userProject;
+        }
+
+        // assign jobs to projects
+        foreach ($userJobs as $job) {
+            // lookup project
+            $project = $userProjectsLookup[$job->project_id];
+
+            // assign job to project
+            $project->addJob($job);
+        }
 
         // assign to view
         $this->view->date           = $date;
