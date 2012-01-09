1fee81645f69bb8c1f9d647bcc9067fe0f119bfd
Guillaume Mathieu <contact@gmathieu.com>
2012-01-09 00:03:50 -0500

    added ajax auto formatter

--- library/App/Controller/Action.php
+++ library/App/Controller/Action.php
@@ -10,15 +10,21 @@ class App_Controller_Action extends Zend_Controller_Action
 
     public function init()
     {
+        parent::init();
+
         $this->controllerName = $this->getRequest()->getControllerName();
         $this->actionName     = $this->getRequest()->getActionName();
         $this->session        = new Zend_Session_Namespace('chronflux');
         $this->_redirector    = $this->_helper->getHelper('Redirector');
 
+        // show grid
         if ($this->_getParam('show-grid')) {
             $url = $this->view->baseUrl('images/24_col.gif');
             $this->view->headStyle()->appendStyle("html { background: #ffffff url({$url}) -5px 0 repeat; }");
         }
+
+        // ajax auto formatter
+        $this->_setAjaxAutoFormat();
     }
 
     public function preDispatch()
@@ -39,5 +45,20 @@ class App_Controller_Action extends Zend_Controller_Action
         $baseUrl    = $this->getFrontController()->getBaseUrl();
 
         return "http://{$servername}{$baseUrl}{$path}";
+    }
+
+    private function _setAjaxAutoFormat()
+    {
+        // check to see if format isn't set and call is ajax
+        if ($this->isAjax() && !$this->_getParam('format')) {
+            $JSONHeader = $this->getRequest()->getHeader('Accept');
+
+            // check the request header to accept json (jQuery only?)
+            if (false === stristr($JSONHeader, 'application/json')) {
+                $this->getRequest()->setParam('format', 'html');
+            } else {
+                $this->getRequest()->setParam('format', 'json');
+            }
+        }
     }
 }
