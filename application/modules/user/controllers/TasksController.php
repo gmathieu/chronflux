6f2a6bd3f5b0d7e7e442a5696b20bb656eb43adb
Guillaume Mathieu <contact@gmathieu.com>
2012-01-08 19:56:37 -0500

    adding task_id param to next_url after a new task is created

--- application/modules/user/controllers/TasksController.php
+++ application/modules/user/controllers/TasksController.php
@@ -2,6 +2,21 @@
 
 class User_TasksController extends App_User_Controller_Settings
 {
+    public function newAction()
+    {
+        $nextUrl = $this->_getParam('next_url');
+        $request = $this->getRequest();
+
+        if ($nextUrl) {
+            $this->_afterSave = function($form, $userDataObj) use ($nextUrl, $request) {
+                // append task_id to next URL
+                $request->setParam('next_url', $nextUrl . '/task_id/' . $userDataObj->task_id);
+            };
+        }
+
+        parent::newAction();
+    }
+
     public function editAction()
     {
         $this->_beforeSave = function($form, $userDataObj) {
